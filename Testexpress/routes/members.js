edb04a537ee12f3426e333fbc95d264f71715d1e
Danielle <p42dmg@mun.ca>
2016-11-13 18:48:39 -0330

    Got members to read from file!!

--- Testexpress/routes/members.js
+++ Testexpress/routes/members.js
@@ -2,39 +2,21 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-/* GET members listing. */
+
 router.get('/', function(req, res, next) {
-	//this way works
-	var members = [
-	           	{
-	        		"name": "Danielle Gillespie",
-	        		"uid": "1",
-	        		"icon": "icon03.png"
-	        	},
-	        	{
-	        		"name": "Joseph Gibson",
-	        		"uid": "2",
-	        		"icon": "icon01.png"
-	        	}
-	        	
-	        ];
-	/* this way doesn't???
+	
+	var members;
 	fs.readFile('data/members.json', 'utf8', function (err, data) {
 	  if (err){
 		  throw err;
 	  }
 	  else{
 		  members = JSON.parse(data);
-		  console.log(members);
-		  console.log(typeof(members));
+		  res.render('members', { 
+			  members: members
+		  });
 	  }
-	});*/
-	
-	  res.render('members', { 
-		  members: members
-	  });
-	  console.log(members);
-	  
+	});
 });
 
 module.exports = router;
