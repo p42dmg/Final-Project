01c3d705214f23e4761e140a280cc6421ffd892b
Danielle <p42dmg@mun.ca>
2016-11-13 21:53:43 -0330

    Added dynamics to profile and friends list

--- Testexpress/routes/profile.js
+++ Testexpress/routes/profile.js
@@ -4,28 +4,51 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
+	function findID(data, idToLookFor) {
+		//console.log(data);
+		console.log("id to find: " + idToLookFor);
+	    for (var i = 0; i < data.length; i++) {
+	    	
+	    	console.log(" user id: " + data[i].uid);
+	        if (data[i].uid == idToLookFor) {
+	        	console.log("data: " + data[i]);
+	            return(data[i]);
+	        }
+	    }
+	}
 	var id = req.query.id;
-	console.log(id);
+	//console.log(id);
 	var members;
 	var profile;
+	var friends;
+	var friendsArray = new Array();
 	fs.readFile('data/members.json', 'utf8', function (err, data) {
 	  if (err){
 		  throw err;
 	  }
 	  else{
-		  console.log(data);
+		 // console.log(data);
 		  members = JSON.parse(data);
 		  for(var i = 0; i < members.length; i++){
 			  if(members[i].uid === id){
 				  profile = members[i];
+				  friends = profile.friends;
+				 // console.log(friends);
 				  break;
 			  }
 		  }
+		  for(i = 0; i < friends.length; i++){
+			 // console.log(friends[i]);
+			  friendsArray.push(findID(members, friends[i]));
+		  }
+		  console.log(friendsArray);
 		  res.render('profile', { 
-			  profile: profile
+			  profile: profile,
+			  friends: friendsArray
 		  });
 	  }
 	});
+	
 });
 
 module.exports = router;
